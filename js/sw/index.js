552a2d0c2b3d3e80125d5d5f7c14543283be9ca9
Menelaos Zetas <menelaoszetas@gmail.com>
2018-05-15 16:00:24 +0300

    SW fixes

--- js/sw/index.js
+++ js/sw/index.js
@@ -6,6 +6,7 @@ self.addEventListener('install', (event) => {
             .then((cache) => {
                 return cache.addAll([
                     '/',
+                    '/index.html',
                     '/restaurant.html',
                     '/css/styles.css',
                     '/images/1-450_small.jpg', '/images/1-600_medium.jpg','/images/1-800_large.jpg',
@@ -30,8 +31,35 @@ self.addEventListener('install', (event) => {
 self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request)
-            .then((response) => {
-                return response || fetch(event.request);
+            .then(function(response) {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+
+                var fetchRequest = event.request.clone();
+
+                return fetch(fetchRequest).then(
+                    function(response) {
+                        // Check if we received a valid response
+                        if(!response || response.status !== 200) {
+                            return response;
+                        }
+
+                        // IMPORTANT: Clone the response. A response is a stream
+                        // and because we want the browser to consume the response
+                        // as well as the cache consuming the response, we need
+                        // to clone it so we have two streams.
+                        let responseToCache = response.clone();
+
+                        caches.open(staticCacheName)
+                            .then(function(cache) {
+                                cache.put(event.request, responseToCache);
+                            });
+
+                        return response;
+                    }
+                );
             })
     )
 });
